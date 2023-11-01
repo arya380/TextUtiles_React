78aa2f3d0e6cee40f7b445cd9b209aff28f31e22
ARYA KUMAR <ar2111sharma@gmail.com>
2023-11-01 15:03:30 +0530

    TextUtiles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+// import About from "./components/About";
+import TextForm from "./components/TextForm";
+import { BrowserRouter as Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Route> */}
+      <Navbar title="Textutils" abouttext="About"></Navbar>
+      <div className="container my-3">
+        {/* <Switch>
+            <Route path="/about">
+              <About />
+            </Route> */}
+
+        {/* <Route exact path="/"> */}
+        {<TextForm heading="Enter The Text To Analyze Below"></TextForm>}
+        {/* </Route> */}
+        {/* </Switch> */}
+      </div>
+      {/* </Route> */}
+    </>
   );
 }
 
